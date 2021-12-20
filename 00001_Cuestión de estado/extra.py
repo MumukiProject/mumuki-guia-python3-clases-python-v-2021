5ed782caf7548d99dd5220e9b5c53e928f671cc1
gustavo@mumuki.org <gustavo@mumuki.org>
2021-12-20 16:37:45 +0000

    Mumuki Export on 2021-12-20 16:37:45 +0000

---
+++ 00001_Cuestión de estado/extra.py
@@ -0,0 +1,15 @@
+class Celular:
+  def __init__(self, bateria, saldo):
+    self.bateria = bateria
+    self.saldo = saldo
+
+  def tiene_bateria_maxima(self):
+    return self.bateria == 100
+
+  def necesita_saldo(self):
+    return self.saldo == 0
+    
+  def cargar_a_tope(self):
+    self.bateria = 100
+
+celular_de_eli = Celular(50, 300)
