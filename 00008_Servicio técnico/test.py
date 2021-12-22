9c0853561cb57095682eafb2284e81e390883047
mayra@mumuki.org <mayra@mumuki.org>
2021-12-22 20:06:50 +0000

    Mumuki Export on 2021-12-22 20:06:50 +0000

--- 00008_Servicio técnico/test.py
+++ 00008_Servicio técnico/test.py
@@ -1,3 +1,3 @@
 
-  def test_el_celular_de_Eli_tiene_"Humanoid 10"_como_sistema_operativo(self):
+  def test_el_celular_de_Eli_tiene_Humanoid_10_como_sistema_operativo(self):
     self.assertTrue(celular_de_eli.bateria, 'Humanoid 10')
