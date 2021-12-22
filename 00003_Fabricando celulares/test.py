2af7e8f3a46c7c6e8f3f5d07a501bda2f690151a
mayra@mumuki.org <mayra@mumuki.org>
2021-12-22 14:40:07 +0000

    Mumuki Export on 2021-12-22 14:40:07 +0000

--- 00003_Fabricando celulares/test.py
+++ 00003_Fabricando celulares/test.py
@@ -1,4 +1,6 @@
-class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_el_celular_de_lu_tiene_inicialmente_50%_de_batería(self):
+    self.assertEqual(celular_de_lu.bateria, 50)
+    
+  def test_el_celular_de_lu_tiene_inicialmente_$80_de_saldo(self):
+    self.assertEqual(celular_de_lu.saldo, 80)
