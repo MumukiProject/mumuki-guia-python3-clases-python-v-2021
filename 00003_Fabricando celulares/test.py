b9568c58cffa45980ebc7780fa54d92c9d869f85
mayra@mumuki.org <mayra@mumuki.org>
2021-12-22 19:34:05 +0000

    Mumuki Export on 2021-12-22 19:34:05 +0000

--- 00003_Fabricando celulares/test.py
+++ 00003_Fabricando celulares/test.py
@@ -2,5 +2,5 @@
   def test_el_celular_de_lu_tiene_inicialmente_50_de_batería(self):
     self.assertEqual(celular_de_lu.bateria, 50)
     
-  def test_el_celular_de_lu_tiene_inicialmente_$80_de_saldo(self):
+  def test_el_celular_de_lu_tiene_inicialmente_80_de_saldo(self):
     self.assertEqual(celular_de_lu.saldo, 80)
