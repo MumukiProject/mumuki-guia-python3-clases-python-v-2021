303b91d581825629ed474dbaa15c7d0dbb048b80
mayra@mumuki.org <mayra@mumuki.org>
2021-12-22 14:55:57 +0000

    Mumuki Export on 2021-12-22 14:55:57 +0000

--- 00007_(A)Tributo al sistema operativo/test.py
+++ 00007_(A)Tributo al sistema operativo/test.py
@@ -1,4 +1,4 @@
-class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_Si_creo_un_nuevo_Celular_le_puedo_especificar_su_sistema_opertivo(self):
+    celular_aristimuño = Celular(0,0,'Humanoid 1.0')
+    self.assertEqual(celular_aristimuño.sistema_operativo,'Humanoid 1.0')
