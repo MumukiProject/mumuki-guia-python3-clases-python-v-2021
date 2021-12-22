2f6f0b901dcd83ec66732a960fc88d2a2c51499e
gustavo@mumuki.org <gustavo@mumuki.org>
2021-12-22 22:05:15 +0000

    Mumuki Export on 2021-12-22 22:05:15 +0000

--- 00012_El gato y la caja/test.py
+++ 00012_El gato y la caja/test.py
@@ -1,4 +1,15 @@
-class Test(unittest.TestCase):
+  
+  def test_cuando_un_gato_come_20_gramos_su_energia_aumenta_20(self):
+    un_gato = Gato(100, 4)
+    un_gato.comer(20)
+    self.assertEqual(un_gato.energia, 120)
+    
+  def test_cuando_un_gato_come_50_gramos_su_energia_aumenta_50(self):
+    un_gato = Gato(150, 8)
+    un_gato.comer(50)
+    self.assertEqual(un_gato.energia, 200))
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_cuando_un_gato_cumple_anios_su_edad_aumenta_en_uno(self):
+    un_gato = Gato(120, 4)
+    un_gato.cumplir_anios()
+    self.assertEqual(un_gato.edad, 5)
