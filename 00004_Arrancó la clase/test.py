0de70c41861e7dd97d4784d191229dc79fdd61ec
mayra@mumuki.org <mayra@mumuki.org>
2021-12-22 14:45:19 +0000

    Mumuki Export on 2021-12-22 14:45:19 +0000

--- 00004_Arrancó la clase/test.py
+++ 00004_Arrancó la clase/test.py
@@ -1,4 +1,8 @@
-class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_Si_creo_un_nuevo_Celular_le_puedo_especificar_su_saldo_inicial(self):
+    celular_aristimuño = Celular(44)
+    self.assertEqual(celular_aristimuño.saldo,44)
+    
+  def test_Si_creo_un_nuevo_Celular_le_puedo_especificar_su_bateria_inicial(self):
+    celular_aristimuño = Celular(100)
+    self.assertEqual(celular_aristimuño.bateria,100)
