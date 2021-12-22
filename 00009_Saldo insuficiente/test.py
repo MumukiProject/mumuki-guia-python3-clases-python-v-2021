a81730718ccc9060c4847824f1c0749b460e4f93
mayra@mumuki.org <mayra@mumuki.org>
2021-12-22 19:35:04 +0000

    Mumuki Export on 2021-12-22 19:35:04 +0000

--- 00009_Saldo insuficiente/test.py
+++ 00009_Saldo insuficiente/test.py
@@ -1,10 +1,10 @@
   
-  def test_Si_le_cargamos_$50_a_un_celular_su_saldo_aumenta_en_50(self):
+  def test_Si_le_cargamos_50_a_un_celular_su_saldo_aumenta_en_50(self):
     celular = Celular(0,0,'Humanoid 10')
     celular.cargar_saldo(50)
     self.assertEqual(celular.saldo,50)
     
-  def test_Si_le_cargamos_$100_a_un_celular_su_saldo_aumenta_en_100(self):
+  def test_Si_le_cargamos_100_a_un_celular_su_saldo_aumenta_en_100(self):
     celular = Celular(0,0,'Humanoid 10')
     celular.cargar_saldo(100)
     self.assertEqual(celular.saldo,100)
