b5b0ad8a9ad0418d4458123396fb58ad46b0f17f
mayra@mumuki.org <mayra@mumuki.org>
2021-12-22 14:51:25 +0000

    Mumuki Export on 2021-12-22 14:51:25 +0000

--- 00006_Comportate, por favor/test.py
+++ 00006_Comportate, por favor/test.py
@@ -1,6 +1,11 @@
 
   def test_un_celular_vuelve_a_tener_100_de_bateria_si_lo_cargo_a_tope(self):
-    celular = Celular(100)
+    celular = Celular(100, 0)
     celular.utilizar(200)
     celular.cargar_a_tope()
     self.assertEqual(celular.bateria,100)
+    
+    
+  def test_un_celular_necesita_saldo_si_tiene_$0(self):
+    celular = Celular(100, 0)
+    self.assertTrue(celular.necesita_saldo())
