e5d1944f9a83a3505123cfcdd809619c83715c4e
mayra@mumuki.org <mayra@mumuki.org>
2021-12-22 15:05:16 +0000

    Mumuki Export on 2021-12-22 15:05:16 +0000

--- 00008_Servicio técnico/test.py
+++ 00008_Servicio técnico/test.py
@@ -1,4 +1,3 @@
-class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_el_celular_de_Eli_tiene_"Humanoid 10"_como_sistema_operativo(self):
+    self.assertTrue(celular_de_eli.bateria, 'Humanoid 10')
