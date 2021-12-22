0a8f2ead1b24853746e4c304f1a1f4a61e6aa08c
mayra@mumuki.org <mayra@mumuki.org>
2021-12-22 14:57:00 +0000

    Mumuki Export on 2021-12-22 14:57:00 +0000

--- 00004_Arrancó la clase/test.py
+++ 00004_Arrancó la clase/test.py
@@ -1,8 +1,8 @@
 
   def test_Si_creo_un_nuevo_Celular_le_puedo_especificar_su_saldo_inicial(self):
-    celular_aristimuño = Celular(44)
+    celular_aristimuño = Celular(0,44)
     self.assertEqual(celular_aristimuño.saldo,44)
     
   def test_Si_creo_un_nuevo_Celular_le_puedo_especificar_su_bateria_inicial(self):
-    celular_aristimuño = Celular(100)
+    celular_aristimuño = Celular(100,0)
     self.assertEqual(celular_aristimuño.bateria,100)
